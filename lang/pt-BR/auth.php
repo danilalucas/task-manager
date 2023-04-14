70218900ab76da0ab9ca7e9a83c304b2cf326c59
Daníla Temoteo Lucas <80535640+danilalucas@users.noreply.github.com>
2023-04-14 16:53:15 -0300

    Add auth

---
+++ lang/pt-BR/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Não encontramos nenhum registro com as credenciais informadas.',
+    'password' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em alguns segundos.',
+
+];
